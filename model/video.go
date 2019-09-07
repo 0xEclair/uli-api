2308f57ce3ffc0d76df5fb23c6a8a9bc35df0b78
eclair-lumiere <38656355+eclair-lumiere@users.noreply.github.com>
2019-09-07 23:02:30 +0800

    update redis_click

--- model/video.go
+++ model/video.go
@@ -3,7 +3,9 @@ package model
 import (
 	"github.com/aliyun/aliyun-oss-go-sdk/oss"
 	"github.com/jinzhu/gorm"
+	"go-crud/cache"
 	"os"
+	"strconv"
 )
 
 // Video 视频模型
@@ -21,4 +23,20 @@ func (video *Video) AvatarURL() string{
 	bucket, _ := client.Bucket(os.Getenv("OSS_BUCKET"))
 	signedGetURL, _ := bucket.SignURL(video.Avatar, oss.HTTPGet, 600)
 	return signedGetURL
+}
+
+// View 点击数
+func (video *Video) View() uint64 {
+	countStr,_:=cache.RedisClient.Get(cache.VideoViewKey(video.ID)).Result()
+	count,_:=strconv.ParseUint(countStr,10,64)
+	return count
+}
+
+// AddView 视频浏览
+func (video *Video) AddView(){
+	// 增加视频点击数
+	cache.RedisClient.Incr(cache.VideoViewKey(video.ID))
+
+	//增加排行榜点击数
+	cache.RedisClient.ZIncrBy(cache.DailyRankKey,1,strconv.Itoa(int(video.ID)))
 }
